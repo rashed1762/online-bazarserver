d2547cf092ef36d452897255cc34c45700d1b9b0
rashed1762 <rashed54islam@gmail.com>
2022-12-05 12:14:31 +0600

    first commit

--- index.js
+++ index.js
@@ -35,64 +35,66 @@ async function run(){
         })
 
         app.put('/user/admin/:email',async(req,res)=>{
-          const email=req.params.email;
-          const requester=req.decoded.email;
-          const requesterAccount=await userCollection.findOne({email:requester});
-          if(requesterAccount.role=== 'admin'){
-            const filter={email:email};
-            const updateDoc={
-              $set:{role:'admin'},
-            };
-            const result=await userCollection.updateOne(filter,updateDoc);
-            res.send(result);
-          }
-          else {
-            res.status(403).send({message:'forbidden'});
-          }
+          const email = req.params.email;
+          const filter = { email: email };
+          const updateDoc = {
+            $set: { role: 'admin' },
+          };
+          const result = await userCollection.updateOne(filter, updateDoc);
+          res.send(result);
        
+        })
+
+        app.get('/admin/:email', async (req, res) => {
+          const email = req.params.email;
+          const user = await userCollection.findOne({ email: email });
+          const isAdmin = user.role === 'admin';
+          res.send({ admin: isAdmin })
         })
          
 
         app.put('/user/:email',async(req,res)=>{
           const email=req.params.email;
-          const user=req.body;
+          const user =req.body;
           const filter={email:email};
-          const options={upsert:true};
-            const updateDoc={
-              $set:user,
-            };
-            const result=await userCollection.updateOne(filter,updateDoc,options);
-            const token=jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'2h'})
-            res.send({result,token});
+          const options ={upsert:true};
+
+          const updateDoc={
+            $set:user,
+          };
+          const result =await userCollection.updateOne(filter,updateDoc,options);
+          const token=jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'2h'})
+          res.send({result,token});
           })
+
+          app.get('/user',async(req,res)=>{
+            const users=await userCollection.find().toArray();
+            res.send(users);
+          }) 
+
           app.post('/cart',async(req,res)=>{
             const booking=req.body;
-            console.log(booking)
-            const query = {value:booking.value,date:booking.date,patient:booking.patient};
             const result=await cartCollection.insertOne(booking);
             res.send(result);
           })
           app.get('/cart', async(req,res)=>{
-            const query={};
-            const cursor=cartCollection.find(query);
-            const products=await cursor.toArray();
-            res.send(products);
+            const useremail=req.query.useremail;
+            const query={useremail:useremail};
+            const result=await cartCollection.find(query).toArray();
+            res.send(result);
   
           });
-          app.get('/cart/:id', async(req,res)=>{
+          app.delete('/cart/:id', async(req,res)=>{
             const id=req.params.id;
-            const query={_id:ObjectId(id)};
-            const result=await cartCollection.deleteOne(query);
-            res.send(result);
+              const filter={_id:ObjectId(id)};
+              const result=await cartCollection.deleteOne(filter);
+              res.send(result);
   
           });
           
           
 
-        app.get('/user',async(req,res)=>{
-          const users=await userCollection.find().toArray();
-          res.send(users);
-        }) 
+       
   
 
     
